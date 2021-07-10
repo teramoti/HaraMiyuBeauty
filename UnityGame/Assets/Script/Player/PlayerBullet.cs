c4fc7e726d58e903b2f974e52563f8afac796ac2
KeishiTeramoto <keishi0315ne@gmail.com>
2021-07-10 12:11:37 +0900

    Playerからカステラを出せるように修正

---
+++ UnityGame/Assets/Script/Player/PlayerBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private float direction;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        //プレイヤーオブジェクトを取得
+        var player = GameObject.FindWithTag("Player");
+
+        direction = player.transform.localScale.x;
+        
+        //5秒後に消滅
+        Destroy(gameObject, 5.0f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        //左に飛んでいく
+        if (direction < 0)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        //右に飛んでいく
+        else
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
